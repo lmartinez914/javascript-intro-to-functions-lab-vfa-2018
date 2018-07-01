f8059d02b7cf414a8b59596e7d7bad5d1c0f25d2
Lorenzo Martinez <lmartinez914@gmail.com>
2018-07-01 03:12:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string) {
-  var response = "Ican't hear you!"
+  var response = "I can't hear you!"
   if (string.toLowerCase()===string) {
     return response
   }
